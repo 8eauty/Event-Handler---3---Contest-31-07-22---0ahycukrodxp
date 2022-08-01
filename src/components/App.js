471d943ad395663c19ed979980d98aa63c74c341
8eauty <106524498+8eauty@users.noreply.github.com>
2022-08-01 09:05:54 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,4 @@
-// import React from 'react'
-// import '../styles/App.css';
-// const App = () => {
 
-//   consthandleInput = (event) =>{
-//     event.preventDefault()
-// console.log(event.target.value)
-//   }
-
-//   // do not change id of input elements
-//   return (
-//     <div id="main">
-//       <label htmlFor='text-input'>Text Input:- </label>
-//       <input id="text-input" type={'text'} onChange={handleInput}/>
-
-//       <br/>
-//       <br/>
-
-//       <label htmlFor='num-input'>Number input</label>
-//       <input id="num-input"  type={'number'}  onChange={handleInput}/>
-//       <br/>
-//     </div>
-//   )
-// }
-
-
-// export default App;
 import React from 'react'
 import '../styles/App.css';
 
